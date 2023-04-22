af9bb65b973e5d1ec446d503a3bd625a7a153bb7
danypepin1 <dany.pepin.1@ulaval.ca>
2023-04-22 00:28:35 -0400

    setup base project components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, SearchFeed, ChannelDetail } from './components';
+
+export const App = () => (
+    <BrowserRouter>
+
+        <Box sx={{background: '#000'}}>
+            <Navbar/>
+            <Routes>
+                <Route path='/'exact element={<Feed/>} />
+                <Route path="/video/:id" element={<VideoDetail/>}></Route>
+                <Route path="/channel/:id" element={<ChannelDetail/>}></Route>
+                <Route path="/video/:searchTerm" element={<SearchFeed/>}></Route>
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
+export default App  
